286ca2a0e34a89571c45955d98ccec914072bf5d
Dan Heneise <danheneise@me.com>
2018-03-10 12:27:53 -0800

    patch_0.1.0: Update RemoteDeploy with gem install.

--- README.md
+++ README.md
@@ -1,3 +1,46 @@
 # DanarchyDeploy
 
 dAnarchy Deploy is a template-driven Ruby gem to deploy locally or remotely to Gentoo systems (and debian/ubuntu, more being added). This can take a .JSON or .YAML input file, or a CouchDB connection as a deployment template and install necessary packages, add users and groups, write ERB templates, and decompress tar/zip archives. More documentation incoming.
+
+
+## Installation
+
+Add this line to your application's Gemfile:
+
+```ruby
+gem 'danarchy_deploy'
+```
+
+And then execute:
+
+    $ bundle
+
+Or install it yourself as:
+
+    $ gem install danarchy_deploy
+
+!! Since dAnarchy Deploy takes several actions that require root/sudo access, I install the gem with sudo as any remote deployment will on the target host:
+
+    $ sudo gem install danarchy_deploy
+
+
+## Usage
+
+dAnarchy Deploy usage info can be read with -h/--help:
+```ruby
+$ danarchy_deploy -h
+Usage: sudo /usr/local/bin/danarchy_deploy (local|remote) --json /path/to/deployment.json [options]
+	-j, --json=file                  Read configuration from JSON file.
+	-y, --yaml=file                  Read configuration from YAML file.
+	-p, --pretend                    Pretend run: Don't take any action.
+	-f, --first-run                  First Run: Run as a first run causing services to run all init actions.
+	-d, --deploy-dir                 Deployment directory. Defaults to '/danarchy/deploy'.
+	--ssh-verbose                Verbose SSH stdout/stderr output.
+	--version                    Print /usr/local/bin/danarchy_deploy version.
+	-h, --help                       Print this help info.
+
+DanarchyDeploy: 0.1.0
+Exiting! Must be run with sudo!
+```
+
+More documentation incoming...
--- lib/danarchy_deploy.rb
+++ lib/danarchy_deploy.rb
@@ -26,7 +26,7 @@ module DanarchyDeploy
       deployment = DanarchyDeploy::Services.init(deployment, options) if deployment[:services]
 
       deployment[:last_deploy] = DateTime.now.strftime("%Y/%m/%d %H:%M:%S")
-      puts "\nFinished Deployment at #{deployment[:last_deploy]}!"
+      puts "\nFinished Local Deployment at #{deployment[:last_deploy]}!"
       File.write(options[:deploy_file], deployment.to_json) if options[:deploy_file].end_with?('.json')
       File.write(options[:deploy_file], deployment.to_yaml) if options[:deploy_file].end_with?('.yaml')
       deployment
@@ -36,13 +36,13 @@ module DanarchyDeploy
   class RemoteDeploy
     def self.new(deployment, options)
       puts "\n" + self.name
+      options[:working_dir] = options[:deploy_dir] + '/' + deployment[:hostname]
       connector = { hostname: deployment[:hostname],
                     ipv4:     deployment[:ipv4],
                     ssh_user: deployment[:ssh_user],
                     ssh_key:  deployment[:ssh_key] }
 
-      gem = build_gem(options)
-      push_gem(connector, gem, options)
+      install_gem(connector, options)
       push_deployment(connector, options)
       deploy_result = remote_LocalDeploy(connector, options)
 
@@ -63,49 +63,9 @@ module DanarchyDeploy
     end
 
     private
-    def self.build_gem(options)
-      gem = "danarchy_deploy-#{DanarchyDeploy::VERSION}.gem"
-      puts " > Building gem: #{gem}"
-      gem_dir = File.expand_path('../../', __FILE__)
-      gem_path = "#{gem_dir}/pkg/#{gem}"
-      build_cmd = "cd #{gem_dir} && rake build"
-      build_cmd = "su - #{ENV['SUDO_USER']} -c '#{build_cmd}'" if ENV['SUDO_USER']
-
-      build_result = DanarchyDeploy::Helpers.run_command(build_cmd, options)
-
-      if build_result[:stderr]
-        puts '   ! Gem build failed!'
-        abort("STDERR:\n#{build_result[:stderr]}")
-      elsif File.exist?(gem_path)
-        puts "   |+ Gem built: #{gem_path}"
-      end
-
-      gem_path
-    end
-
-    def self.push_gem(connector, gem, options)
-      puts "\n > Creating directory #{options[:deploy_dir]}."
-      mkdir_result = DanarchyDeploy::Helpers.run_command("ssh -o StrictHostKeyChecking=no -i #{connector[:ssh_key]} #{connector[:ssh_user]}@#{connector[:ipv4]} 'test -d #{options[:deploy_dir]}/#{connector[:hostname]} || sudo mkdir -vp #{options[:deploy_dir]}/#{connector[:hostname]} && sudo chown -Rc #{connector[:ssh_user]}:#{connector[:ssh_user]} #{options[:deploy_dir]}'", options)
-
-      if mkdir_result[:stderr]
-        puts '   ! Directory creation failed!'
-        abort("STDERR:\n#{mkdir_result[:stderr]}")
-      else
-        puts "   |+ Created directory: #{options[:deploy_dir]}"
-      end
-
-      puts "\n > Pushing gem: #{gem} to #{connector[:hostname]}"
-      push_result = DanarchyDeploy::Helpers.run_command("scp -i #{connector[:ssh_key]} #{gem} #{connector[:ssh_user]}@#{connector[:ipv4]}:#{options[:deploy_dir]}/", options)
-
-      if push_result[:stderr]
-        puts '   ! Gem push failed!'
-        abort("STDERR:\n#{push_result[:stderr]}")
-      else
-        puts "   |+ Gem pushed!"
-      end
-      
-      puts "\n > Installing gem: #{gem} on #{connector[:hostname]}"
-      install_result = DanarchyDeploy::Helpers.run_command("ssh -i #{connector[:ssh_key]} #{connector[:ssh_user]}@#{connector[:ipv4]} 'sudo gem install -f #{options[:deploy_dir]}/#{File.basename(gem)}'", options)
+    def self.install_gem(connector, options)
+      puts "\n > Installing danarchy_deploy on #{connector[:hostname]}"
+      install_result = DanarchyDeploy::Helpers.run_command("ssh -i #{connector[:ssh_key]} #{connector[:ssh_user]}@#{connector[:ipv4]} -o ConnectTimeout=30 'sudo gem install -f danarchy_deploy && test -d #{options[:working_dir]} || sudo mkdir -vp #{options[:working_dir]} && sudo chown -Rc #{connector[:ssh_user]}:#{connector[:ssh_user]} #{options[:working_dir]}'", options)
 
       if install_result[:stderr]
         puts '   ! Gem install failed!'
@@ -118,8 +78,8 @@ module DanarchyDeploy
     # Add ssh-keygen function, then update 
     def self.push_deployment(connector, options)
       puts "\n > Pushing deployment: #{options[:deploy_file]}"
-      # push_result = DanarchyDeploy::Helpers.run_command("rsync --rsh 'ssh -i #{connector[:ssh_key]}' -Havu --delete #{options[:deploy_dir]}/#{connector[:hostname]}/ #{connector[:ssh_user]}@#{connector[:ipv4]}:#{options[:deploy_dir]}/#{connector[:hostname]}", options)
-      push_result = DanarchyDeploy::Helpers.run_command("scp -i #{connector[:ssh_key]} #{options[:deploy_file]} #{connector[:ssh_user]}@#{connector[:ipv4]}:#{options[:deploy_dir]}/#{connector[:hostname]}/", options)
+      push_result = DanarchyDeploy::Helpers.run_command("rsync --rsh 'ssh -i #{connector[:ssh_key]}' -Havzu --delete #{options[:working_dir]}/ #{connector[:ssh_user]}@#{connector[:ipv4]}:#{options[:working_dir]}/", options)
+      # push_result = DanarchyDeploy::Helpers.run_command("scp -i #{connector[:ssh_key]} #{options[:deploy_file]} #{connector[:ssh_user]}@#{connector[:ipv4]}:#{options[:deploy_dir]}/#{connector[:hostname]}/", options)
       
       if push_result[:stderr]
         puts '   ! Deployment push failed!'
@@ -157,7 +117,7 @@ module DanarchyDeploy
 
     def self.remote_Cleanup(connector, options)
       puts "\n > Cleaning up: #{options[:deploy_dir]}"
-      cleanup_result = DanarchyDeploy::Helpers.run_command("ssh -i #{connector[:ssh_key]} #{connector[:ssh_user]}@#{connector[:ipv4]} 'sudo rm -rfv #{options[:deploy_dir]}'", options)
+      cleanup_result = DanarchyDeploy::Helpers.run_command("ssh -i #{connector[:ssh_key]} #{connector[:ssh_user]}@#{connector[:ipv4]} 'sudo rm -rfv #{options[:working_dir]}'", options)
 
       if cleanup_result[:stderr]
         puts '   ! Deployment cleanup failed!'
