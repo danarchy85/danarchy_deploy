56cfdf3959550737a3d371909ae6f68c911745e1
Dan James <dan@danarchy.me>
2024-09-11 21:24:32 -0700

    fix hostnamectl command

--- lib/danarchy_deploy/system/debian.rb
+++ lib/danarchy_deploy/system/debian.rb
@@ -60,7 +60,7 @@ module DanarchyDeploy
 
       private
       def set_hostname(hostname)
-        `hostnamectl hostname #{hostname}`
+        `hostnamectl set-hostname #{hostname}`
       end
     end
   end
